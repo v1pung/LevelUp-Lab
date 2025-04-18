eb180ffc6c494b4ecd2f8ef94d8c0d430e42ec78
v1pung <kaby.spb1@gmail.com>
2025-04-18 19:55:31 +0300

    Fix scheduler not updating ranks properly

--- app/scheduler.py
+++ app/scheduler.py
@@ -18,22 +18,30 @@ def save_xp():
         for user in users:
             compl_activities = Activity.query.filter_by(owner=user.id, is_completed=True).all()
             total_xp = sum(activity.reward for activity in compl_activities) if compl_activities else -5
-            user.rank += total_xp if user.rank > 5 else 0
+
+            if total_xp < 0 and user.rank < 5:
+                user.rank += 0
+            else:
+                user.rank += total_xp
 
             for activity in compl_activities:
-                existing_activity = RankHistory.query.filter_by(activity_id=activity.id, date=today).all()
+                existing_activity = RankHistory.query.filter_by(activity_id=activity.id, user_id=user.id, date=today).first()
                 if not existing_activity:
                     new_entry = RankHistory(user_id=user.id, activity_id=activity.id, date=today, value=activity.reward)
+                    db.session.add(new_entry)
 
             try:
-                db.session.add(new_entry)
                 db.session.commit()
-                print(f"[{datetime.now()}] XP сохранены для всех пользователей.")
+                print(f"[{datetime.now()}] XP сохранены для пользователя {user.id}.")
             except Exception as e:
+                db.session.rollback()
                 print("BD ERROR " + str(e))
-            else:
-                for activity in compl_activities:
-                    activity.is_completed = False
+
+            # Сброс completed статусов
+            for activity in compl_activities:
+                activity.is_completed = False
+            db.session.commit()
 
 
-scheduler.add_job(id='save_xp_job', func=save_xp, trigger='cron', hour=18, minute=58, misfire_grace_time=60)
+
+scheduler.add_job(id='save_xp_job', func=save_xp, trigger='cron', hour=00, minute=00, misfire_grace_time=60)
